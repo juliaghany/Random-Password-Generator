bfe2f990fd9b95d8a22026d5f182a83a8c620023
Jules <juliaghany@msn.com>
2023-02-08 21:27:51 -0600

    remove if statements because they are unnecessary

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // Assignment Code
 
-// Should I use a charset instead? 
-// Do I need to put quotation marks around each letter in the vars with letters? 
-
 var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var lowerCaseCharacters = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCaseCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialCharacters = ["&", "!", "$", "(", ")", "*", "+", "-", "/", ":", "=", "?", ">", "<", "@"];
 
 
@@ -17,26 +14,16 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
-
+// Do I need to add another part to the function that pertains to whether or not the user selects cancel? 
 function generatePassword() {
   var numberOfCharacters = prompt("How many characters would you like in your password?")
-  if (numberOfCharacters <= 12) 
-    var useNumbers = prompt("Would you like to use numbers?")
-  if (useNumbers = "yes")
-    var useLowerCase = prompt("Would you like to use lower case letters?")
-  if (useLowerCase = "yes")
-    var useUpperCase = prompt("Would you like to use upper case letters?")
-  if (useUpperCase = "yes")
-    var useSpecialCharacters = prompt("Would you like to use special characters?")
-  //if (useSpecialCharacters = "yes")
-}
-
-function writePassword() {
-  
+  var useNumbers = prompt("Would you like to use numbers?")
+  var useLowerCase = prompt("Would you like to use lower case letters?")
+  var useUpperCase = prompt("Would you like to use upper case letters?")
+  var useSpecialCharacters = prompt("Would you like to use special characters?")
 }
 
 
@@ -44,9 +31,9 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-//Create arrays for each type of characters--numbers, uppers, lowers, special
-//Prompt the user for input about # and types of characters
-//Capture those inputs in variables
+//Create arrays for each type of characters--numbers, uppers, lowers, special (x)
+//Prompt the user for input about # and types of characters (x)
+//Capture those inputs in variables (x)
 //Create a pool of characters to use based on each response--so if/then statements with each response/answer, use.concat to add chosen character pools to master array
 //Use a for loop that loops the number of characters chosen by the student and at each iteration use Math.floor(Math.random for the index of the master array to choose a random character and add it to the password string
 //After the loop, return the generated password.
